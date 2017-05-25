5f0f1b6ca0036d5c75499448c11b8d8c50ecfade
janethugarcia <janet.huacahuasi.garcia@gmail.com>
2017-05-25 18:08:49 -0500

    feat(slider): se maqueto

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,52 @@
 "use strict";
 
-var a = function a() {
-  return console.log(":)");
-};
+// var a = () => console.log(":)");
+var app = function () {
+	var st = void 0,
+	    dom = void 0,
+	    catchDom = void 0,
+	    fn = void 0,
+	    event = void 0,
+	    suscribeEvents = void 0,
+	    initialize = void 0;
+
+	st = {
+		slider: ".js-slidul",
+		controlLeft: ".js-slidul-control-left",
+		controlRight: ".js-slidul-control-right"
+	};
+	dom = {};
+	catchDom = function catchDom() {
+		dom.slider = $(st.slider);
+		dom.controlLeft = $(st.controlLeft);
+		dom.controlRight = $(st.controlRight);
+	};
+	suscribeEvents = function suscribeEvents() {
+		dom.controlLeft.on("click", event.onClickPreviousImage);
+		dom.controlRight.on("click", event.onClickNextImage);
+	};
+	event = {};
+	event.onClickPreviousImage = function () {
+		fn.getPreviousImage();
+	};
+	event.onClickNextImage = function () {
+		fn.getNextImage();
+	};
+	fn = {};
+	fn.getPreviousImage = function () {
+		alert("previous");
+	};
+	fn.getNextImage = function () {
+		alert("next");
+	};
+	initialize = function initialize() {
+		catchDom();
+		suscribeEvents();
+		console.log("[run MOD slidul]");
+	};
+	return {
+		init: initialize
+	};
+}();
+
+app.init();
--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,44 @@
-var a = () => console.log(":)");
+// var a = () => console.log(":)");
+var app = (()=>{
+	let st, dom, catchDom, fn, event, suscribeEvents, initialize;
+
+	st = {
+		slider: ".js-slidul",
+		controlLeft : ".js-slidul-control-left",
+		controlRight : ".js-slidul-control-right"
+	}
+	dom = {};
+	catchDom = ()=>{
+		dom.slider = $( st.slider );
+		dom.controlLeft = $( st.controlLeft );
+		dom.controlRight = $( st.controlRight );
+	};
+	suscribeEvents = ()=>{
+		dom.controlLeft.on( "click", event.onClickPreviousImage );
+		dom.controlRight.on( "click", event.onClickNextImage );
+	};
+	event = {};
+	event.onClickPreviousImage = ()=>{
+		fn.getPreviousImage();
+	};
+	event.onClickNextImage = ()=>{
+		fn.getNextImage();
+	};
+	fn = {};
+	fn.getPreviousImage = ()=>{
+		alert("previous");
+	};
+	fn.getNextImage = ()=>{
+		alert("next");	
+	};
+	initialize = ()=>{
+		catchDom();
+		suscribeEvents();
+		console.log("[run MOD slidul]");	
+	};
+	return {
+		init : initialize
+	}
+})()
+
+app.init();
